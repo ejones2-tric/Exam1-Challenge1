7396c4f6032564e3683a9b74c2897104e3977c0e
Student <Student@DESKTOP-PT84J89>
2020-02-05 15:12:58 -0500

    Add project files.

---
+++ Exam1 Challenge1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1_Challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Weight in Pounds");
+            string userWeight = Console.ReadLine();
+            int intWeight = Int32.Parse(userWeight);
+            Console.Write("Enter Height in Inches");
+            string userHeight = Console.ReadLine();
+            int intHeight = Int32.Parse(userHeight);
+            int result = intWeight / intHeight ^ 2;
+            Console.WriteLine("Your BMI is "  + result);  
+            Console.ReadLine();
+            if (result < 18.5)
+        {
+                Console.WriteLine("Your BMI indicates you are Underweight");
+           
+             
+            }
+            
+        }
+    }
+}
